c0ad41a0c61e7fd16208be549572d130392b8984
Kevin Lanni <therealklanni@gmail.com>
2019-02-11 21:34:39 -0700

    chore(header): fix unit test

--- libs/component/header/src/lib/header/header.component.spec.ts
+++ libs/component/header/src/lib/header/header.component.spec.ts
@@ -1,16 +1,25 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'mat-toolbar,mat-toolbar-row',
+  template: '<ng-content></ng-content>'
+})
+class MockMaterialComponent { }
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+    TestBed
+      .configureTestingModule({
+        declarations: [HeaderComponent, MockMaterialComponent]
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
