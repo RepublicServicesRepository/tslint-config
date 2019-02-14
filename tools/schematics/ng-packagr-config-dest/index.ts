f08d2e526b94c99e3c4488dd5cc5d07c8795d5b3
Kevin Lanni <therealklanni@gmail.com>
2019-02-13 20:17:55 -0700

    chore(tooling): add schematic for fixing configs
    Fixes the `dest` value in ng-package.json files because
    I couldn't figure out how to configure this as a default.

---
+++ tools/schematics/ng-packagr-config-dest/index.ts
@@ -0,0 +1,29 @@
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { PathFragment } from '@angular-devkit/core';
+
+const destination = './dist/';
+
+export default function (schema: any): Rule {
+  return chain([
+    function (tree: Tree, _context: SchematicContext) {
+      let path: string;
+
+      if (schema.path.startsWith('libs/') || schema.path.startsWith('./libs/')) {
+        path = schema.path;
+      } else {
+        path = `libs/${schema.path}`;
+      }
+
+      const filePath = `${path}/ng-package.json` as PathFragment;
+      const config = JSON.parse(tree.root.file(filePath).content.toString());
+
+      if (config.dest !== destination) {
+        config.dest = destination;
+
+        tree.overwrite(filePath, JSON.stringify(config, null, 2));
+      }
+
+      return tree;
+    }
+  ]);
+}
