8e69a183b952a4231b362173e5fece857287c1b0
Kevin Lanni <therealklanni@gmail.com>
2019-02-18 16:23:27 -0700

    chore(tooling): enforce commit subject length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [1, 'always', 50],
+    'header-max-length': [2, 'always', 50],
     'body-max-line-length': [2, 'always', 72],
     'footer-max-line-length': [2, 'always', 72],
     'type-enum': [2, 'always', [
