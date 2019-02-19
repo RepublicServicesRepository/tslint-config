b7241e55db58ae703b10a0bc46f07777c7930b47
Kevin Lanni <therealklanni@gmail.com>
2019-02-19 16:32:05 -0700

    feat: switch to peer deps

--- libs/tslint-config/index.js
+++ libs/tslint-config/index.js
@@ -1,44 +1,29 @@
 module.exports = {
-  'rulesDirectory': [
-    'codelyzer',
-    'tslint-plugin-prettier'
+  rulesDirectory: ['codelyzer', 'tslint-plugin-prettier'],
+  extends: [
+    'tslint:recommended',
+    'tslint-eslint-rules',
+    'tslint-config-prettier'
   ],
-  'rules': {
-    'prettier/prettier': [
-      2,
-      {
-        'singleQuote': true,
-        'printWidth': 100
-      }
-    ],
+  rules: {
+    prettier: [2, { singleQuote: true, printWidth: 100 }],
     'arrow-return-shorthand': true,
     'binary-expression-operand-order': true,
     'callable-types': true,
     'class-name': true,
-    'comment-format': [
-      true,
-      'check-space'
-    ],
-    'curly': true,
-    'deprecation': {
-      'severity': 'warn'
-    },
-    'forin': true,
-    'handle-callback-err': [
-      true,
-      '^.*(e|E)rr'
-    ],
-    'interface-name': [
-      true,
-      'never-prefix'
-    ],
+    'comment-format': [true, 'check-space'],
+    curly: true,
+    deprecation: { severity: 'warn' },
+    forin: true,
+    'handle-callback-err': [true, '^.*(e|E)rr'],
+    'interface-name': [true, 'never-prefix'],
     'interface-over-type-literal': true,
     'label-position': true,
     'member-access': false,
     'member-ordering': [
       true,
       {
-        'order': [
+        order: [
           'static-field',
           'instance-field',
           'static-method',
@@ -49,24 +34,14 @@ module.exports = {
     'newline-before-return': true,
     'no-arg': true,
     'no-bitwise': true,
-    'no-console': [
-      true,
-      'debug',
-      'info',
-      'time',
-      'timeEnd',
-      'trace'
-    ],
+    'no-console': [true, 'debug', 'info', 'time', 'timeEnd', 'trace'],
     'no-construct': true,
     'no-debugger': true,
     'no-duplicate-super': true,
     'no-empty': false,
     'no-empty-interface': true,
     'no-eval': true,
-    'no-inferrable-types': [
-      true,
-      'ignore-params'
-    ],
+    'no-inferrable-types': [true, 'ignore-params'],
     'no-invalid-this': true,
     'no-misused-new': true,
     'no-non-null-assertion': true,
@@ -80,26 +55,13 @@ module.exports = {
     'no-var-keyword': true,
     'object-literal-sort-keys': false,
     'prefer-const': true,
-    'radix': true,
-    'triple-equals': [
-      true,
-      'allow-null-check'
-    ],
+    radix: true,
+    'triple-equals': [true, 'allow-null-check'],
     'unified-signatures': true,
     'valid-jsdoc': true,
     'variable-name': false,
-    'directive-selector': [
-      true,
-      'attribute',
-      'app',
-      'camelCase'
-    ],
-    'component-selector': [
-      true,
-      'element',
-      'app',
-      'kebab-case'
-    ],
+    'directive-selector': [true, 'attribute', 'app', 'camelCase'],
+    'component-selector': [true, 'element', 'app', 'kebab-case'],
     'no-output-on-prefix': true,
     'use-input-property-decorator': true,
     'use-output-property-decorator': true,
@@ -110,15 +72,5 @@ module.exports = {
     'use-pipe-transform-interface': true,
     'component-class-suffix': true,
     'directive-class-suffix': true
-  },
-  'extends': [
-    'tslint:recommended',
-    'tslint-eslint-rules',
-    'tslint-config-prettier'
-  ],
-  'linterOptions': {
-    'exclude': [
-      ' /**/polyfills.ts'
-    ]
   }
-}
+};
